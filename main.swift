10429f9f6c1bedc1fb2f5148a154169861f8b191
harpreet1994 <39779232+harpreet1994@users.noreply.github.com>
2018-07-19 16:42:54 -0400

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  Shopping
+//
+//  Created by MacStudent on 2018-07-19.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+var tejas = Customer()
+
+print(tejas.displayData())
+
+var jatinder = Customer(customerID: 101, customerName: "Jatinder", address: "Attmar Dr. Brampton", email: "Jatinder@mad.com", creditCardInfo: "4520-0100-1234-5678", shippingInfo: "Ship at lambton college between 1PM to 5PM")
+print("\(jatinder.displayData())")
+
+tejas.CustomerID = 102
+tejas.CustomerName = "Tejas"
+tejas.Address = "oshawa"
+tejas.Email = "Tejas@mad.com"
+tejas.CreditCaredInfo = "4520-0100-9876-7865"
+tejas.ShippingInfo = "Don 't Deliver between 1PM to 5PM"
+print(tejas.displayData())
+
+var navdeep = Customer()
+navdeep.registerUser()
+print(navdeep.displayData())
